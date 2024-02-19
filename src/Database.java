b3f8d580535e602c8300f7c2703a552c98361a65
Philipe Souza <philipe.souza@student.kdg.be>
2024-02-19 16:04:48 +0100

    Login and database for login

--- src/Database.java
+++ src/Database.java
@@ -1,4 +1,46 @@
+import java.sql.*;
+
 public class Database {
-    private int idG;
+    private int idGame;
     private int idPlayer;
+
+    private Connection connection;
+
+    public Database() {
+    }
+
+
+    public boolean isUsernameInDatabase(String login){
+
+        String verifyUsername = "SELECT login FROM logs_info WHERE login = ?";
+        try (PreparedStatement statement = connection.prepareStatement(verifyUsername)) {
+            statement.setString(1, login);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                return resultSet.next(); // If there is a next result, the login exists
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public boolean isLoginCorrect(String login, String password){
+        String verifyLogin = "SELECT password FROM logs_info WHERE login = ?";
+        if(!isUsernameInDatabase(login))
+            return false;
+
+        try(PreparedStatement statement = connection.prepareStatement(verifyLogin)){
+            statement.setString(1, login);
+            try (ResultSet resultSet = statement.executeQuery()) {
+
+                resultSet.next(); //perhaps I don't need this
+                String pass = resultSet.getString("password");
+                return password.equals(pass);
+            }
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
