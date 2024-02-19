459a4efc5d3e1e863f8bebc035b55f3082168b94
Souhaib Othmani <souhaib.othmani@student.kdg.be>
2024-02-19 13:45:04 +0100

    fix3

---
+++ src/Login.java
@@ -0,0 +1,10 @@
+public class Login {
+    public boolean checkUsername(String username){
+        boolean checku = false;
+        return checku;
+    }
+    public boolean checkPass(String username,String password){
+        boolean checkp = false;
+        return checkp;
+    }
+}
