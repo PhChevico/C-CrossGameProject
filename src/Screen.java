bfe3c3bc4a0a71c153c6d6abdb9b2cbb2e31b300
Souhaib Othmani <souhaib.othmani@student.kdg.be>
2024-02-26 16:31:45 +0100

    Comments

--- src/Screen.java
+++ src/Screen.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Screen {
     private Scanner scanner = new Scanner(System.in);
-    public int LoginScreen(){
+    public int LoginScreen(){ //we ask the user if they want to either login or register and we return the choice
         int choice;
         System.out.println("enter \n1-LOGIN \n2-REGISTER");
         choice = scanner.nextInt();
