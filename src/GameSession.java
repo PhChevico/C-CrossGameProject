86f6c6a95617c7453d6b10fda564cae8015c8946
Philipe Souza <philipe.souza@student.kdg.be>
2024-02-26 13:36:26 +0100

    Setting game session + Initiating player class development.

--- src/GameSession.java
+++ src/GameSession.java
@@ -1,3 +1,8 @@
 public class GameSession {
     private String time;
+    Authentication authentication = new Authentication();
+    Board board = new Board();
+    Database database = new Database();
+    Player player = new Player();
+
 }
--- src/Player.java
+++ src/Player.java
@@ -3,4 +3,26 @@ public class Player {
     private int pawnNumber;
     private int nOfWins;
     private final int MAX_NUMB_PAWNS = 25;
+
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPawnNumber() {
+        return pawnNumber;
+    }
+
+    public void updatePawnNumber(int insertedPawnNumber) {
+        pawnNumber getPawnNumber() - insertedPawnNumber;
+
+    }
+
+    //methods when conquering zones...
+
+
 }
