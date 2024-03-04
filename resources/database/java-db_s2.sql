c3f30a51870a6a73f988ef5c5d94d4308c663d29
Philipe Souza <philipe.souza@student.kdg.be>
2024-03-04 12:32:56 +0100

    Created the database + resources folder with the sql + added to lib the .jar file + created two tables for the database (logs-info) and (game_statistics)

---
+++ resources/database/java-db_s2.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS logs_info cascade ;
+DROP TABLE IF EXISTS game_statistics cascade ;
+DROP SEQUENCE IF EXISTS seq_game_id;
+
+
+CREATE TABLE IF NOT EXISTS logs_info (
+                                        name VARCHAR(15) constraint pk_logs_info primary key,
+                                        password VARCHAR(25)
+);
+
+
+CREATE TABLE IF NOT EXISTS game_statistics(
+                                        id SERIAL PRIMARY KEY , --game id
+                                        name VARCHAR(15) constraint fk_name_logs_info references logs_info(name), --winner name
+                                        time_played TIMESTAMP NOT NULL, --total time played
+                                        total_turns NUMERIC(2)--input the total turn used by the winner
+);
+
+
+
+
+
+
+
+
