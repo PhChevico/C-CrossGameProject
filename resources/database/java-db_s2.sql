8f2e9fd063d0a06e73603fd66ef2625586fca814
Philipe Souza <philipe.souza@student.kdg.be>
2024-03-04 14:23:12 +0100

    Create database argument.

--- resources/database/java-db_s2.sql
+++ resources/database/java-db_s2.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE c_cross_team21;
+
+
 DROP TABLE IF EXISTS logs_info cascade ;
 DROP TABLE IF EXISTS game_statistics cascade ;
 DROP SEQUENCE IF EXISTS seq_game_id;
@@ -13,10 +16,9 @@ CREATE TABLE IF NOT EXISTS game_statistics(
                                         id SERIAL PRIMARY KEY , --game id
                                         name VARCHAR(15) constraint fk_name_logs_info references logs_info(name), --winner name
                                         time_played TIMESTAMP NOT NULL, --total time played
-                                        total_turns NUMERIC(2)--input the total turn used by the winner
+                                        total_turns_p1 NUMERIC(2),--input the total turn used by the winner
+                                        total_turns_p2 NUMERIC(2)--input the total turn used by the winner
 );
-
-
 
 
 
