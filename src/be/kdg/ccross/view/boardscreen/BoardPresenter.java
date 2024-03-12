c6245b4258b37b8f4820f5e7133bc727e7776980
Philipe Souza <philipe.souza@student.kdg.be>
2024-03-12 18:52:42 +0100

    Fixing position of methods and attributes in boardMaker. Added event when click on square Image.

--- src/be/kdg/ccross/view/boardscreen/BoardPresenter.java
+++ src/be/kdg/ccross/view/boardscreen/BoardPresenter.java
@@ -1,6 +1,10 @@
 package be.kdg.ccross.view.boardscreen;
 import be.kdg.ccross.model.GameSession;
+import javafx.scene.control.Alert;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+
+import java.util.List;
 
 
 public class BoardPresenter {
@@ -13,15 +17,21 @@ public class BoardPresenter {
 
         // Show initial board
         this.view.boardMaker(session.getSquaresAsList());
-
         this.addEventHandlers();
     }
 
 
     private void addEventHandlers(){
-        view.squares.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
-            System.out.println("javafx sucks");
-            mouseEvent.consume();
-        });
+
+        for (ImageView img : view.getBoardImages()) {
+            img.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "No HI");
+                System.out.println("javafx is cool");
+                System.out.println(img.getLayoutX());
+                System.out.println(img.getLayoutY());
+                mouseEvent.consume();
+            });
+        }
     }
+
 }
--- src/be/kdg/ccross/view/boardscreen/BoardView.java
+++ src/be/kdg/ccross/view/boardscreen/BoardView.java
@@ -1,15 +1,25 @@
 package be.kdg.ccross.view.boardscreen;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class BoardView extends GridPane {
 
-    final ImageView squares = new ImageView(new Image("images/squareTest.jpg"));
+    static final String IMAGES_SQUARE_TEST_JPG = "images/squareTest.jpg";
     private final int SQUARE_SIZE = 85;
+
+    List<ImageView> boardImages = new ArrayList<>();
+
+    public List<ImageView> getBoardImages() {
+        return boardImages;
+    }
 
     public BoardView(){
         this.initialiseNodes();
@@ -21,6 +31,9 @@ public class BoardView extends GridPane {
 
 
     private void layoutNodes() {
+//        GridPane gridPane = new GridPane();
+//        gridPane.setAlignment(Pos.CENTER);
+
         this.setAlignment(Pos.CENTER);
         BackgroundImage backgroundImage = new BackgroundImage(
                 new Image("images/bg1.jpg", false),
@@ -32,6 +45,7 @@ public class BoardView extends GridPane {
 
     }
 
+
     public void boardMaker(List<String> boardAsArray) {
 
         for (String boardAsList : boardAsArray) {
@@ -40,11 +54,13 @@ public class BoardView extends GridPane {
                 String[] parts = boardAsList.split("-");
 
                 //creating the image and setting the size of the images
-                ImageView img = new ImageView(squares.getImage());
+                ImageView img = new ImageView(IMAGES_SQUARE_TEST_JPG);
                 img.setFitWidth(SQUARE_SIZE);
                 img.setFitHeight(SQUARE_SIZE);
                 //adding the image to the view
-                this.add(img, Integer.parseInt(parts[1]), Integer.parseInt(parts[0]));
+                add(img, Integer.parseInt(parts[1]), Integer.parseInt(parts[0]));
+                getBoardImages().add(img);
+
             }
         }
     }
