4a73f0c85f73c17c0b802e62f8cf227bf64139d0
Philipe Souza <philipe.souza@student.kdg.be>
2024-03-18 14:44:50 +0100

    Fixing the problem of clicking on a pawn and having the same event.

--- src/be/kdg/ccross/view/boardscreen/BoardPresenter.java
+++ src/be/kdg/ccross/view/boardscreen/BoardPresenter.java
@@ -1,6 +1,7 @@
 package be.kdg.ccross.view.boardscreen;
 import be.kdg.ccross.model.GameSession;
 import javafx.event.Event;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
@@ -8,9 +9,11 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javafx.util.Pair;
 
 import java.util.Iterator;
 import java.util.Map;
+import java.util.function.Consumer;
 
 
 public class BoardPresenter {
@@ -24,49 +27,53 @@ public class BoardPresenter {
         // Show initial board
         this.view.boardMaker(session.getSquaresAsList());
         this.addEventHandlers();
+
+
     }
 
 
-    void addEventHandlers(){
+    void addEventHandlers() {
 
 
         //return the coordinates of the square.
         for (Map.Entry<String, ImageView> entry : view.getBoardImages().entrySet()) {
-            ImageView img = entry.getValue();
-            img.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
-
+            ImageView squareImg = entry.getValue();
+            squareImg.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
                 System.out.println(entry.getKey());
-
-                //if zone not dominated
-                if(!session.getBoard().getSquare(entry.getKey()).isStatus()){
-                    session.getBoard().getSquare(entry.getKey()).setStatus(true);
-
-                    //here we put a pawn into the square
-                    String[] parts = entry.getKey().split("-");
-                    view.add(view.createPawn(entry.getKey()), Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
-                    System.out.println(session.getBoard().getSquare(entry.getKey()).isStatus());
-                } else {
-                    //if zone dominated
-                    System.out.println("Impossible to put a pawn in that square");
-                }
-
-
-                // gives the key corresponding to the clicked image
-                //session.handleClickBoard(entry.getKey());
-
+                checkingActionToSquare(new Pair<>(entry.getKey(), entry.getValue()));
                 mouseEvent.consume();
             });
         }
 
+        // Add event listener
+        view.setHandlerOnPawnCreated((Pair<String, ImageView> pair) -> {
+            pair.getValue().addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+                checkingActionToSquare(pair);
+            });
+        });
 
-        //put a pawn into the square
 
 
         view.getScene().getWindow().setOnCloseRequest(this::closeApplication);
 
+
+
     }
 
+    private void checkingActionToSquare(Pair<String, ImageView> pair) {
+        //if zone not dominated
+        if (!session.getBoard().getSquare(pair.getKey()).isStatus()) {
+            session.getBoard().getSquare(pair.getKey()).setStatus(true);
 
+            //here we put a pawn pair the square
+            String[] parts = pair.getKey().split("-");
+            view.add(view.createPawn(pair.getKey()), Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
+            System.out.println(session.getBoard().getSquare(pair.getKey()).isStatus());
+        } else {
+            //if zone dominated
+            System.out.println("Impossible to put a pawn in that square");
+        }
+    }
 
 
     void closeApplication(Event event) {
