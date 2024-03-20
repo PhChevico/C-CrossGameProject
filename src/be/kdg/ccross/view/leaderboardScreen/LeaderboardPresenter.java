3c5ce7958a8f928a30b853c419cefa79deae245c
Souhiab Othmani <souhaib.othmani@student.kdg.be>
2024-03-20 16:49:57 +0100

    Leaderboard Screen with sample data

--- src/be/kdg/ccross/view/leaderboardScreen/LeaderboardPresenter.java
+++ src/be/kdg/ccross/view/leaderboardScreen/LeaderboardPresenter.java
@@ -1,8 +1,28 @@
 package be.kdg.ccross.view.leaderboardScreen;
 
+import be.kdg.ccross.model.Database;
 import be.kdg.ccross.model.GameSession;
+import be.kdg.ccross.model.PlayerStatistics;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class LeaderboardPresenter {
-    GameSession model;
-    LeaderboardView view;
+    private GameSession model;
+    private LeaderboardView view;
+    private Database database;
+
+    public LeaderboardPresenter(GameSession model, LeaderboardView view) {
+        this.model = model;
+        this.view = view;
+        this.database = new Database(); // Create an instance of the Database class
+        updateView(); // Call updateView to initially populate the leaderboard
+    }
+
+    public void updateView() {
+        // Fetch player statistics from the database
+        ObservableList<PlayerStatistics> playerStatisticsList = FXCollections.observableArrayList(database.getPlayerStatistics());
+
+        // Update the view with the fetched player statistics
+        view.updateLeaderboard(playerStatisticsList);
+    }
 }
--- src/be/kdg/ccross/view/leaderboardScreen/LeaderboardView.java
+++ src/be/kdg/ccross/view/leaderboardScreen/LeaderboardView.java
@@ -1,35 +1,74 @@
 package be.kdg.ccross.view.leaderboardScreen;
 
+import be.kdg.ccross.model.PlayerStatistics;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-public class LeaderboardView extends GridPane {
-    private Label rank;
-    private Label playerName;
-    private Label gamesPlayed;
-    private Label numberOfWins;
-    private Label numberOfLoss;
-    private Label percentageGamesWon;
-    private Label numberOfMoves;
-    private Label averageDuration;
+public class LeaderboardView extends VBox {
     private Button goBack;
-    public LeaderboardView(){
+
+    public LeaderboardView() {
         initialiseNodes();
         layoutNodes();
     }
-    private void initialiseNodes(){
-        rank = new Label();
-        playerName = new Label();
-        gamesPlayed = new Label();
-        numberOfWins = new Label();
-        numberOfLoss = new Label();
-        percentageGamesWon = new Label();
-        numberOfMoves = new Label();
-        averageDuration = new Label();
-        goBack = new Button("Go back");
-    }
-    private void layoutNodes(){
 
+    private void initialiseNodes() {
+        goBack = new Button("Go back");
+        goBack.setStyle("-fx-font-size: 14px; -fx-padding: 8px 16px;");
+    }
+
+    private void layoutNodes() {
+        this.setSpacing(10);
+        this.setAlignment(Pos.CENTER);
+
+        GridPane headerPane = new GridPane();
+        headerPane.setHgap(10);
+        headerPane.setPadding(new Insets(10));
+        headerPane.setAlignment(Pos.CENTER);
+
+        headerPane.add(new Label("Rank"), 0, 0);
+        headerPane.add(new Label("Player Name"), 1, 0);
+        headerPane.add(new Label("Games Played"), 2, 0);
+        headerPane.add(new Label("Wins"), 3, 0);
+        headerPane.add(new Label("Losses"), 4, 0);
+        headerPane.add(new Label("% Games Won"), 5, 0);
+        headerPane.add(new Label("Avg Moves"), 6, 0);
+        headerPane.add(new Label("Avg Duration"), 7, 0);
+
+        GridPane dataPane = new GridPane();
+        dataPane.setHgap(10);
+        dataPane.setVgap(5);
+        dataPane.setPadding(new Insets(10));
+        dataPane.setAlignment(Pos.CENTER);
+
+        // Add the "Go back" button spanning all columns
+        HBox buttonPane = new HBox(goBack);
+        buttonPane.setAlignment(Pos.CENTER);
+
+        this.getChildren().addAll(headerPane, dataPane, buttonPane);
+    }
+
+    // Method to update the leaderboard with the fetched player statistics
+    public void updateLeaderboard(ObservableList<PlayerStatistics> playerStatisticsList) {
+        GridPane dataPane = (GridPane) this.getChildren().get(1);
+        dataPane.getChildren().clear(); // Clear previous data
+
+        int rowIndex = 1; // Start from the second row after header
+        for (PlayerStatistics stats : playerStatisticsList) {
+            dataPane.add(new Label(stats.getPlayerName()), 1, rowIndex);
+            dataPane.add(new Label(String.valueOf(stats.getGamesPlayed())), 2, rowIndex);
+            dataPane.add(new Label(String.valueOf(stats.getWins())), 3, rowIndex);
+            dataPane.add(new Label(String.valueOf(stats.getLosses())), 4, rowIndex);
+            dataPane.add(new Label(String.valueOf(stats.getWinPercentage())), 5, rowIndex);
+            dataPane.add(new Label(String.valueOf(stats.getAvgMoves())), 6, rowIndex);
+            dataPane.add(new Label(String.valueOf(stats.getAvgDuration())), 7, rowIndex);
+            rowIndex++; // Move to the next row
+        }
     }
 }
