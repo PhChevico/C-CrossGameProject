29ea82b5f5af919267de80b9e798aac04e5a74a1
Reno-Peeters <Reno.Peeters@student.kdg.be>
2024-04-22 09:32:31 +0200

    NOT NULL added to a bunch of stuff

--- resources/database/java-db_s2.sql
+++ resources/database/java-db_s2.sql
@@ -9,29 +9,30 @@ DROP SEQUENCE IF EXISTS seq_game_id;
 
 
 CREATE TABLE IF NOT EXISTS logs_info (
-                                        name VARCHAR(15),
-                                        username VARCHAR(15) constraint pk_username primary key,
-                                        password VARCHAR(25)
+                                        name VARCHAR(15) NOT NULL ,
+                                        username VARCHAR(15) NOT NULL constraint pk_username primary key,
+                                        password VARCHAR(25) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Players (
-                         player_id INT PRIMARY KEY,
-                         player_name VARCHAR(255)
+                         player_id INT PRIMARY KEY NOT NULL ,
+                         player_name VARCHAR (255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Games (
-                       game_id INT PRIMARY KEY,
-                       start_time TIMESTAMP,
-                       end_time TIMESTAMP,
-                       winner_id INT,
-                       total_play_time TIME
+                       game_id INT PRIMARY KEY NOT NULL ,
+                       start_time TIMESTAMP NOT NULL ,
+                       end_time TIMESTAMP NOT NULL ,
+                       winner_id INT NOT NULL ,
+                       total_play_time TIME NOT NULL ,
+                       FOREIGN KEY (winner_id) REFERENCES Players(player_id)
 );
 
 CREATE TABLE IF NOT EXISTS Moves (
-                       move_id INT PRIMARY KEY,
-                       game_id INT,
-                       player_id INT,
-                       move_time TIME,
+                       move_id INT PRIMARY KEY NOT NULL ,
+                       game_id INT NOT NULL ,
+                       player_id INT NOT NULL ,
+                       move_time TIME NOT NULL ,
                        FOREIGN KEY (game_id) REFERENCES Games(game_id),
                        FOREIGN KEY (player_id) REFERENCES Players(player_id)
 );
